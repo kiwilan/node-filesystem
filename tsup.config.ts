05ec5550b88e03e7ebcc216d4ed1167dfcdf79f7
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-03-02 15:29:37 +0100

    init

---
+++ tsup.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  name: 'fastify-utils',
+  entry: {
+    index: 'src/index.ts',
+    syn: 'src/syn.ts',
+  },
+  format: ['esm', 'cjs'],
+  // outExtension({ format }) {
+  //   return {
+  //     js: `.${format}.js`,
+  //   }
+  // },
+  dts: true,
+  minify: true,
+  treeshake: true,
+  splitting: true,
+  external: [
+    'fastify',
+    'esbuild',
+    '@fastify/env',
+    '@fastify/middie',
+    '@fastify/cors',
+  ],
+})
